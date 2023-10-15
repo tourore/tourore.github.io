dda32f695f2b04e73ca7e38dda76f4baa641cc52
Shloimie <hshloimie@gmail.com>
2023-10-14 22:49:18 -0400

    pub 3

--- script_init.js
+++ script_init.js
@@ -156,8 +156,9 @@ function switch_to_dic(dict){
          }}
          html += back_button_card(org ,back_text, parent)
         html += cards_html
-    window.scrollTo({ top: 0, behavior: 'smooth' });
+
         reset_page(html)
+    window.scrollTo({ top: 0, behavior: 'smooth' });
   // console.log(org);
 
 
