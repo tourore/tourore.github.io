ad86ec4b200cc83897522f169c3180c7aee3cd38
Shloimie <hshloimie@gmail.com>
2023-10-19 19:44:46 -0400

    physics

--- script_init.js
+++ script_init.js
@@ -93,21 +93,39 @@ function get_back_button_path(str) {
   return ht
 }
 function back_button_card(text, path, parent){
-    if (text != '/'){
-        // console.log(text)
-    return `    <div style="display: flex; justify-content: left; ">
-        <button  onclick="switch_to_dic(${path})" type="button" class="btn btn-secondary file_card"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-left" viewBox="0 0 16 16">
-  <path fill-rule="evenodd" d="M15 8a.5.5 0 0 0-.5-.5H2.707l3.147-3.146a.5.5 0 1 0-.708-.708l-4 4a.5.5 0 0 0 0 .708l4 4a.5.5 0 0 0 .708-.708L2.707 8.5H14.5A.5.5 0 0 0 15 8z"/>
-</svg> ${parent} </button>
-        <div class="card col-lg-10 thick-border"" style="display: inline-block;  margin-left: 30px; margin-right:30px;">
-  <div class="card-body col-lg-10 " style="display: flex padding-left: 30px; text-align: center; ">
-  <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="card-title bi bi-folder2 inline" viewBox="0 0 16 16">
-      <path d="M1 3.5A1.5 1.5 0 0 1 2.5 2h2.764c.958 0 1.76.56 2.311 1.184C7.985 3.648 8.48 4 9 4h4.5A1.5 1.5 0 0 1 15 5.5v7a1.5 1.5 0 0 1-1.5 1.5h-11A1.5 1.5 0 0 1 1 12.5v-9zM2.5 3a.5.5 0 0 0-.5.5V6h12v-.5a.5.5 0 0 0-.5-.5H9c-.964 0-1.71-.629-2.174-1.154C6.374 3.334 5.82 3 5.264 3H2.5zM14 7H2v5.5a.5.5 0 0 0 .5.5h11a.5.5 0 0 0 .5-.5V7z"/>
-    </svg>
-    <strong class="title-case" >${text}</strong> 
-  </div>
+    if (text != '/') {
+        if (window.innerWidth >= 700) {
+            // console.log(text)
+            return `    <div style="display: flex; justify-content: left; ">
+                    <button  onclick="switch_to_dic(${path})" type="button" class="btn btn-secondary file_card"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-left" viewBox="0 0 16 16">
+              <path fill-rule="evenodd" d="M15 8a.5.5 0 0 0-.5-.5H2.707l3.147-3.146a.5.5 0 1 0-.708-.708l-4 4a.5.5 0 0 0 0 .708l4 4a.5.5 0 0 0 .708-.708L2.707 8.5H14.5A.5.5 0 0 0 15 8z"/>
+            </svg> ${parent} </button>
+                    <div class="card col-lg-10 thick-border"" style="display: inline-block;  margin-left: 30px; margin-right:30px;">
+              <div class="card-body col-lg-10 " style="display: flex padding-left: 30px; text-align: center; ">
+              <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="card-title bi bi-folder2 inline" viewBox="0 0 16 16">
+                  <path d="M1 3.5A1.5 1.5 0 0 1 2.5 2h2.764c.958 0 1.76.56 2.311 1.184C7.985 3.648 8.48 4 9 4h4.5A1.5 1.5 0 0 1 15 5.5v7a1.5 1.5 0 0 1-1.5 1.5h-11A1.5 1.5 0 0 1 1 12.5v-9zM2.5 3a.5.5 0 0 0-.5.5V6h12v-.5a.5.5 0 0 0-.5-.5H9c-.964 0-1.71-.629-2.174-1.154C6.374 3.334 5.82 3 5.264 3H2.5zM14 7H2v5.5a.5.5 0 0 0 .5.5h11a.5.5 0 0 0 .5-.5V7z"/>
+                </svg>
+                <strong class="title-case" >${text}</strong> 
+              </div>
+            </div>
+                </div>`
+        } else {return `<div style="display: flex; flex-direction: column; align-items: left;">
+    <button onclick="switch_to_dic(${path})" type="button" class="btn btn-secondary file_card col-sm-12 col-sm-6">
+        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-left" viewBox="0 0 16 16">
+            <path fill-rule="evenodd" d="M15 8a.5.5 0 0 0-.5-.5H2.707l3.147-3.146a.5.5 0 1 0-.708-.708l-4 4a.5.5 0 0 0 0 .708l4 4a.5.5 0 0 0 .708-.708L2.707 8.5H14.5A.5.5 0 0 0 15 8z"/>
+        </svg> ${parent}
+    </button>
+    <div class="card col-lg-10 thick-border" style="display: inline-block; margin-left: 30px; margin-right: 30px;">
+        <div class="card-body col-lg-10" style="text-align: center;">
+            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="card-title bi bi-folder2 inline" viewBox="0 0 16 16">
+                <path d="M1 3.5A1.5 1.5 0 0 1 2.5 2h2.764c.958 0 1.76.56 2.311 1.184C7.985 3.648 8.48 4 9 4h4.5A1.5 1.5 0 0 1 15 5.5v7a1.5 1.5 0 0 1-1.5 1.5h-11A1.5 1.5 0 0 1 1 12.5v-9zM2.5 3a.5.5 0 0 0-.5.5V6h12v-.5a.5.5 0 0 0-.5-.5H9c-.964 0-1.71-.629-2.174-1.154C6.374 3.334 5.82 3 5.264 3H2.5zM14 7H2v5.5a.5.5 0 0 0 .5.5h11a.5.5 0 0 0 .5-.5V7z"/>
+            </svg>
+            <strong class="title-case">${text}</strong>
+        </div>
+    </div>
 </div>
-    </div>`}
+`}
+    }
     else {return ''}
 }
 
